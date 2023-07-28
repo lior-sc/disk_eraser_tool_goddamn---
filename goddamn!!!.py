2d533581d92948f3deda923c63afb3f1814b2969
lior Schwartz <liorsmail89@gmail.com>
2023-07-28 16:41:38 +0300

    Added explanation to the code

--- goddamn!!!.py
+++ goddamn!!!.py
@@ -1,3 +1,17 @@
+'''
+This code is used in order to scan the amount of available space on a drive and than 
+overwrite it with the word "goddamn!!!". 
+
+The reason for doing so is that this operation alongside formatting will reduce the 
+ability of a third party to restore data from a hard drive after conducting formatting 
+of the drive.
+
+I've used this code in order to sell an old computer without compromising my data, which
+was present on the hard drive prior to formatting it
+
+good luck and goddamn! https://youtu.be/PTqtrGBYpxQ
+'''
+
 import shutil
 
 tot,used,free=shutil.disk_usage("/")
@@ -11,7 +25,7 @@ except:
 print("number of bytes entered is: {} [bytes]".format(number_of_bytes))
 
 if(number_of_bytes < int(free) and number_of_bytes > 0):
-    print("Creating required file. goddamn!!!")
+    print("Creating required file. goddamn!!!\n")
     
     with open("goddamn!!!.txt", "wb") as binary_file:
         byte_2_write = b'goddamn!!!'
@@ -22,7 +36,7 @@ if(number_of_bytes < int(free) and number_of_bytes > 0):
             if(i%1e7 == 0):
                 print("{} MB written".format(int(i*1e-5)),end= "\n")
         
-        print("Finished!")
+        print("\n\nFinished!")
         binary_file.close()
     
         
